66397f5a3efceeff0040dd9cc781c38fd755a413
root <yxchi0704@gmail.com>
2022-02-16 19:58:18 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -25,9 +25,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
-  tags = {
-    Department = "devops"
-  }
 }
 
 resource "azurerm_subnet" "subnet" {
