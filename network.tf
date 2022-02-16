b4bee6677c40554c5794f01df95d3afec3da357a
root <yxchi0704@gmail.com>
2022-02-16 20:19:23 +0000

    added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/practice-gates/network/azurerm"
   version = "3.5.0"
-  resource_group_name = ${var.prefix}-workshop
+  resource_group_name = "${var.prefix}-workshop"
 # insert required variables here
 }
